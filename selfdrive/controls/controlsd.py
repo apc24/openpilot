9e76cabe46086f6faac7e36216acd468fc86db00
APC2-4Gr <apc24pilot@gmail.com>
2026-06-17 01:02:27 +0000

    modify

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -3,7 +3,7 @@ import os
 import math
 import time
 import threading
-from typing import SupportsFloat
+from typing import Any, SupportsFloat
 
 import cereal.messaging as messaging
 
@@ -63,7 +63,7 @@ ENABLED_STATES = (State.preEnabled, *ACTIVE_STATES)
 
 class CarD:
   CI: CarInterfaceBase
-  CS: car.CarState
+  CS: Any
 
   def __init__(self, CI=None):
     self.can_sock = messaging.sub_sock('can', timeout=20)
@@ -90,7 +90,7 @@ class CarD:
     """Initialize CarInterface, once controls are ready"""
     self.CI.init(self.CP, self.can_sock, self.pm.sock['sendcan'])
 
-  def state_update(self, CC: car.CarControl):
+  def state_update(self, CC: Any):
     """carState update loop, driven by can"""
 
     # TODO: This should not depend on carControl
@@ -136,7 +136,7 @@ class CarD:
       cp_send.carParams = self.CP
       self.pm.send('carParams', cp_send)
 
-  def controls_update(self, CC: car.CarControl):
+  def controls_update(self, CC: Any):
     """control update loop, driven by carControl"""
 
     # send car controls over can
